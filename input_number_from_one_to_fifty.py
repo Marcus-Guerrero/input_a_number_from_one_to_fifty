be361cc3c2f73ddc37f975a7a50410299b65271f
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-28 18:41:02 +0800

    Creating the Pseodocode

---
+++ input_number_from_one_to_fifty.py
@@ -0,0 +1,9 @@
+#State a list
+#Place a while loop
+#Place another while loop for the input and conditions
+#Create a statement that ask users to input a number from 1-50
+#State conditions wherein a user can only put numbers from 1-50
+#Break the second while loop
+#Create another statement that ask users if they want to add more entries, if not, break the first loop
+#Print all the values inputted by arrange in various ranges from 1-10...all the way to 41-50
+
