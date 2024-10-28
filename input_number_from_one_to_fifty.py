41f4fd799a796838de626ac211a2b96e2349859b
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-28 21:39:49 +0800

    Fixing some previous issues

--- input_number_from_one_to_fifty.py
+++ input_number_from_one_to_fifty.py
@@ -15,8 +15,8 @@ while True:
         else:
             print("We cannot accept any number that is greater than or less than the range of 1-50, please try again")
 
-    value_archive[range_values]={
-
+    value_archive={
+        "coded_value": range_values
     }
 
     while True:
@@ -39,24 +39,28 @@ thirty_one_to_forty = []
 forty_one_to_fifty = []
 
 for value, digit in value_archive.items():
-    if 1<= value <= 10:
-        one_to_ten_values.append(value)
-        one_to_ten_values = [value]
-    elif 11<= value <= 20:
-        eleven_to_twenty.append(value)
-        eleven_to_twenty = [value]
-    elif 21<= value <= 30:
-        twenty_one_to_thirty.append(value)
-        twenty_one_to_thirty = [value]
-    elif 31<= value <= 40:
-        thirty_one_to_forty.append(value)
-    elif 41<= value <= 50:
-        forty_one_to_fifty.append(value)
-        forty_one_to_fifty = [value]
+    if 1<= digit <= 10:
+        one_to_ten_values.append(digit)
+        one_to_ten_values = [digit]
+    elif 11<= digit <= 20:
+        eleven_to_twenty.append(digit)
+        eleven_to_twenty = [digit]
+    elif 21<= digit <= 30:
+        twenty_one_to_thirty.append(digit)
+        twenty_one_to_thirty = [digit]
+    elif 31<= digit <= 40:
+        thirty_one_to_forty.append(digit)
+    elif 41<= digit <= 50:
+        forty_one_to_fifty.append(digit)
+        forty_one_to_fifty = [digit]
 
 print (one_to_ten_values)
-print (eleven_to_twenty)
 
 
 #Print all the values inputted by arrange in various ranges from 1-10...all the way to 41-50
 
+#range_1= one_to_ten_values.count(range_values)
+#range_2= eleven_to_twenty.count(range_values)
+#range_3= twenty_one_to_thirty.count(range_values)
+#range_4= thirty_one_to_forty.count(range_values)
+#range_5= forty_one_to_fifty.count(range_values)
