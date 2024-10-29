b01607a0fd0696762f0b4dba99078298645d9b5e
Marcus Guerrero <marcus.guerrero61@gmail.com>
2024-10-29 16:37:38 +0800

    Fixing minor issues

--- input_number_from_one_to_fifty.py
+++ input_number_from_one_to_fifty.py
@@ -15,6 +15,7 @@ while True:
         else:
             print("We cannot accept any number that is greater than or less than the range of 1-50, please try again")
 
+    #Store the values to the general list
     value_archive.append(range_values)
 
     while True:
@@ -24,19 +25,20 @@ while True:
             print("Input another number again")
             break
         elif another_entry == "n":
-            break
+            break #Break the loop for reentry
         elif another_entry != "y" and another_entry != "n":
             print ("Wrong command prompt format, please input (y/n) only")
     if another_entry == "n":
-        break
+        break #Break the final loop
 
+#Place five list for storing designated values stated in the conditions
 result_1= []
 result_2= []
 result_3= []
 result_4= []
 result_5= []
 
-for val in value_archive:
+for val in value_archive: # State the conditions for sorting the values in different list
     if 1<= val <=10:
         result_1.append(val)
     elif 11<= val <= 20:
@@ -48,14 +50,16 @@ for val in value_archive:
     else:
         result_5.append(val)
 
+#Place them in five different variables for organization
 val_1=(len(result_1))
 val_2=(len(result_2))
 val_3=(len(result_3))
 val_4=(len(result_4))
 val_5=(len(result_5))
 
+#Print all the values inputted by arrange in various ranges from 1-10...all the way to 41-50
 print (f" 1-10: {val_1} \n 11-20: {val_2} \n 21-30: {val_3} \n 31-40: {val_4} \n 41-50: {val_5}")
 
 
-#Print all the values inputted by arrange in various ranges from 1-10...all the way to 41-50
+
 
